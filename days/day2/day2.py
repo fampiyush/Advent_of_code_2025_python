94e10a52aab469918d222fe51f4ebdda4c672766
Piyush Gupta <piyushgupta941d@gmail.com>
2025-12-02 17:55:49 +0530

    Adds day2 part1

---
+++ days/day2/day2.py
@@ -0,0 +1,39 @@
+import os
+
+input_file_path = os.path.join(os.path.dirname(__file__), 'day2.txt')
+
+example_input = '''11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124'''
+
+def day2_part1():
+    ranges = []
+    try:
+        with open(input_file_path, 'r') as file:
+            content = file.read()
+            ranges = content.split(",")
+    except FileNotFoundError:
+        print(f"Error: The file '{input_file_path}' was not found.")
+
+    sum = 0
+
+    # ranges = example_input.split(",")
+
+    for ran in ranges:
+        start, end = ran.split("-")
+        
+        for i in range(int(start), int(end)+1):
+            str_i = str(i)
+            length = len(str_i)
+            if length%2 == 0 and str_i[:int(length/2)] == str_i[int(length/2):]:
+                sum += i
+    
+    return sum
+
+def day2_part2():
+    pass
+
+if __name__ == "__main__":
+    result = day2_part1()
+    # result = day2_part2()
+    print(result)
